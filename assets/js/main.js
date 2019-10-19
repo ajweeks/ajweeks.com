a24e13fa77ff2b43987ca780018c20be976ae7a9
AJ Weeks <ajweeks@shaw.ca>
2019-10-19 13:22:32 +0100

    Set header color randomly on page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});});
+
+function onLoad() {
+  var r = Math.random() * 90  + 60;
+  var g = Math.random() * 115 + 60;
+  var b = Math.random() * 110 + 60;
+  $('#page-header').css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
+}
